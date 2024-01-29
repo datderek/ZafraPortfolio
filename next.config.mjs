e91870c5ba471445d83bca22981b92fe86ba4b5f
Derek Tran <tran.derek.d.@gmail.com>
2024-01-28 20:43:30 -0800

    Configure Nextjs for static app

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    /* 
+     * Enables static exports for static webapps
+     *
+     * https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+     * 
+     */
+    output: 'export',
+
+    /*
+     * Deploys Next.js app under the sub-path of a domain (Github Pages)
+     *
+     * https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+     */
+    basePath: "/zafraportfolio",
+
+    /*
+     * Disable server-based image optimization, as we won't be using a custom
+     * loader
+     *
+     * https://nextjs.org/docs/app/building-your-application/deploying/static-exports#image-optimization
+     */
+    images: {
+        unoptimized: true,
+    }
+};
 
 export default nextConfig;
