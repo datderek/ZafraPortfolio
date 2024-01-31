8cd7115ee6509b5224f25051ea1cd67393793766
Derek Tran <tran.derek.d@gmail.com>
2024-01-30 21:39:16 -0800

    Add hover message and resize logo with media query

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,10 @@ export default function Home() {
   return (
     <div>
       <NavBar></NavBar>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-[url('../public/cover.png')] bg-cover">
+      <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-[url('../public/cover.png')] bg-cover bg-center">
+        <div className='opacity-0 hover:opacity-100 fixed top-1/4 left-1/3 h-3/4 w-1/3 transition duration-300'>
+          <img className='h-36 fixed top-1/4 left-1/2' src='/zafraportfolio/arrow.svg'/>
+        </div>
       </main>
     </div>
   );
